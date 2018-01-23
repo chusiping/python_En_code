5acb4ff83e4f00ac8e3e37d5df492f48cf478b3b
chusiping <chusiping@163.com>
2018-01-23 11:28:01 +0800

    4 加入python安装和使用说明.doc

--- python35/pass_py/Pass_TestTensorFlow.py
+++ python35/pass_py/Pass_TestTensorFlow.py
@@ -2,3 +2,5 @@ import tensorflow as tf
 hello = tf.constant('Hello, TensorFlow!')
 sess = tf.Session()
 print(sess.run(hello))
+
+# 测试命令：(root) E:\python_En_code\python35>python pass_py/pass_testtensorFlow.py
