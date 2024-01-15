123e03d5763ce5dfa4cf14b73ca04b6d90fceae2
chusiping <chusiping@qq.com>
2024-01-15 18:05:06 +0800

    2024-1-15 18:05 yh

--- 视频分割/cut_video分割视频.py
+++ 视频分割/cut_video分割视频.py
@@ -1,5 +1,9 @@
 import ffmpy
 import os
+import subprocess
+import sys
+
+ffmpeg_path = r'D:\lvse\ffmpeg\bin\ffmpeg.exe'
 
 def convert_to_seconds(time_str):
     hours, minutes, seconds = map(int, time_str.split(':'))
@@ -14,15 +18,39 @@ def convert_to_time(seconds):
     time_str = f"{hours:02d}:{minutes:02d}:{seconds:02d}"
     return time_str
 
+def get_video_duration(video_path):
+    # 调用FFmpeg命令获取视频信息
+    command = [ffmpeg_path, '-i', video_path]
+    result = subprocess.run(command, capture_output=True, text=True)
+
+    # 从输出中提取视频时长信息
+    output = result.stderr
+    duration_line = [line for line in output.split('\n') if 'Duration' in line][0]
+    duration = duration_line.split('Duration: ')[1].split(',')[0]
+    duration = duration.split('.')[0]
+
+    return duration
+
 sourceFile = input("请输入视频文件：").replace('"', '')
-time_str1 = input("请输入视频总时长（格式为 HH:MM:SS）：")
+time_str1 = get_video_duration(sourceFile)   
 time_str2 = input("请输入第二个时间值（格式为 HH:MM:SS）：")
+     
 
-
-# 测试用
 # sourceFile = r'C:\Users\Administrator\Desktop\新建文件夹\a.mp4'
-# time_str1 ='00:48:15'
+# time_str1 = get_video_duration(sourceFile)   
 # time_str2 ='00:10:00'
+
+print("")
+print("")
+print(f'指定文件：{sourceFile}')
+print(f'文件时长：{time_str1}')
+print(f'分割时长：{time_str2}')
+
+proceed = input("是否开始执行？(y/n): ")
+
+if proceed.lower() != 'y':
+    print("已取消执行。")
+    sys.exit(0)  
 
 
 # 将用户输入的时间值转换为整数
@@ -37,7 +65,7 @@ if remainder != 0:
     c += 1
 
 
-ffmpeg_path = r'D:\lvse\ffmpeg\bin\ffmpeg.exe'
+
 
 for i in range(c):
     star = i*b
