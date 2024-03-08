bf68dca1b4756000c4ccbff84e2f86f2228c3823
chusiping <chusiping@qq.com>
2024-03-08 14:08:52 +0800

    2024-3-8 14:08  edit  取消header参数

--- 接口测试curl/OA自动化/OA自动登录启用账号.py
+++ 接口测试curl/OA自动化/OA自动登录启用账号.py
@@ -12,10 +12,6 @@ session=requests.Session()
 
 url = 'http://172.18.197.10:9099/api/hrm/systeminfo/save'
 
-headers = {
-    "Content-Type": "application/x-www-form-urlencoded",
-    "Cookie": "ecology_JSessionid=aaadj4X1UH9Ofhmpj-43y; JSESSIONID=aaadj4X1UH9Ofhmpj-43y; languageidweaver=7; loginuuids=1; loginidweaver=sysadmin; __randcode__=834790dc-61d6-4168-8dab-01a34b155a49"
-}
 
 def send_curl_request():
 
@@ -37,7 +33,7 @@ def send_curl_request():
         "isRememberPassword": "false"
     }
 
-    response = session.post(login_url, data=login_data,headers=headers)
+    response = session.post(login_url, data=login_data)
     decoded_data = response.content.decode('unicode-escape').encode('latin1').decode('utf8')
     json_data = json.loads(decoded_data)
     print(decoded_data)
@@ -47,7 +43,7 @@ def send_curl_request():
     if response.status_code == 200:
         for i in range(1):
                 url_ = 'http://172.18.197.10:9099/weaver/weaver.file.MakeValidateCode?isView=1&validatetype=0&validatenum=4&seriesnum_=1709689420493'
-                user_input = 常用类库.显示在线验证码图_session方式(session,url_,headers)
+                user_input = 常用类库.显示在线验证码图_session方式(session,url_)
                 payload={'id': '110954','needauto': '0','enableDate': '2024-03-06','enableUsbType': '0','loginid': '00191514','passwordlock': '0','password': 'qwertyuiop','password1': 'qwertyuiop','clAuthtype': '','usbstate': '0','tokenKey': '','serial': '','startUsing': '','seclevel': '60','useSecondaryPwd': '0','validatecode': user_input }
                 print(payload)
                 response = session.post(url, data=payload)
