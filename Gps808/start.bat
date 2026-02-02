86656aac49ee07dc9b8f5774ecd5c01e2bf76110
chusiping <chusping@163.com>
2026-02-02 18:16:02 +0800

    2026-2-2 18:15  yh

--- Gps808/start.bat
+++ Gps808/start.bat
@@ -1,10 +1,20 @@
-@echo off
+@REM @echo off
 chcp 65001 >nul
-cd /e E:\git_15home\python_En_code_git\Gps808
 
-start "" /b python main_v2.py ^
-  --excel "excle\车充轨迹.xlsx" ^
-  --phone 13301110130 ^
-  --server-ip 14.23.86.188 ^
-  --server-port 6608 ^
-  --no-send >> run.log 2>&1
+REM 获取标准时间 YYYYMMDDhhmmss
+for /f "tokens=2 delims==" %%i in ('wmic os get localdatetime /value') do set dt=%%i
+
+set "LOG_FILE=logs\run_%dt:~0,4%-%dt:~4,2%-%dt:~6,2%_%dt:~8,2%-%dt:~10,2%-%dt:~12,2%.log"
+
+start "" /b python task.py ^
+      --no-send >> "%LOG_FILE%" 2>&1
+
+@REM start "" /b python main_v2.py ^
+@REM   --excel "excle\车充轨迹.xlsx" ^
+@REM   --phone 13301110130 ^
+@REM   --server-ip 14.23.86.188 ^
+@REM   --server-port 6608 ^
+@REM   --no-send >> "%LOG_FILE%" 2>&1
+
+@REM tasklist | findstr python
+@REM taskkill /F /IM python.exe
