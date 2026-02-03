a0deefe713026c10f2e87d9dd5b9c447bd9c36ea
chusiping <chusping@163.com>
2026-02-03 16:44:09 +0800

    2026年2月3日16:43:55 yh

--- Gps808/main_v2.py
+++ Gps808/main_v2.py
@@ -195,9 +195,10 @@ def main():
                 brake_on = True
             if 'ACC关闭' in excel_data[i][5]:
                 status = 2      #2 ACC 关闭 + 定位有效
-                speed = _speed
+                speed = _speed  # 速度为零
             if 'ACC开启' in excel_data[i][5] and '未定位' in excel_data[i][5] :
                 status = 1      #1 ACC开启 + 未定位
+                speed = _speed  # 速度为零
             
             # 增加判断两条数据之间的时间差秒，用来模仿真实数据的停顿
             if i + 1 < total_rows:
