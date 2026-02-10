986b6976bd035aab4e1b542d82560281d8998708
chusiping <chusping@163.com>
2026-02-10 16:40:58 +0800

    2026-2-10 16:40 yh

--- bazi/lib.py
+++ bazi/lib.py
@@ -1,15 +1,3 @@
-bazi = {
-    "year":  "戊午",
-    "month": "壬寅",
-    "day":   "庚申",
-    "hour":  "戊寅",
-    "year_stem":  "戊",
-    "year_branch":"午",
-    "month_stem": "壬",
-    "month_branch":"寅",
-    "day_stem":   "庚",
-    "day_branch": "申",
-}
 
 # 四废日
 SI_FEI_RI = {
@@ -24,14 +12,34 @@ SI_FEI_RI = {
             "day": {"壬子","癸亥"}
         }
     ],
-    "intro": "四废日：月令与日柱相废"
+    "intro": "月令与日柱相废"
+}
+# 魁罡
+KUI_GANG = {
+    "name": "魁罡",
+    "rules": [
+        {
+            "day": {"庚辰", "庚戌", "壬辰", "壬戌"}
+        }
+    ],
+    "intro": "日柱为庚辰、庚戌、壬辰、壬戌者，为魁罡日"
 }
 
+# 神煞
 ALL_GODS = [
-    SI_FEI_RI,
-    # YANG_REN,
-    # KUI_GANG,
+    SI_FEI_RI,  #四废日
+    KUI_GANG    #魁罡
 ]
+# 取神煞
+def find_gods(bazi):
+    result = []
+    for g in ALL_GODS:
+        if match_rules(bazi, g["rules"]):
+            result.append({
+                "name": g["name"],
+                "intro": g.get("intro", "")
+            })
+    return result
 
 def match_rule_group(bazi: dict, rule: dict) -> bool:
     """单组 AND 规则"""
@@ -52,3 +60,27 @@ def match_rules(bazi: dict, rules: list) -> bool:
 def is_si_fei_ri(bazi: dict) -> bool:
     return match_rules(bazi, SI_FEI_RI["rules"])
 
+if __name__ == "__main__":
+    bazi = {
+    "year":  "戊午",
+    "month": "壬寅",
+    "day":   "庚申",
+    "hour":  "戊寅",
+    "year_stem":  "戊",
+    "year_branch":"午",
+    "month_stem": "壬",
+    "month_branch":"寅",
+    "day_stem":   "庚",
+    "day_branch": "申",
+    }
+    bazi_Arr = [
+        {"month_branch":"寅","day":"庚申"},
+        {"day":"庚辰"},
+        {"month_branch":"子","day":"甲子"},
+    ]
+    
+    for _bazi in bazi_Arr:
+        gods = find_gods(_bazi)
+        for g in gods:
+            print(f"{g['name']}：{g['intro']}")
+        # print(f"{g['name']}：{g['intro']}")
