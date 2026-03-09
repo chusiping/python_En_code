6a4c7c545800a183490fcdb42c5723a4bc4c1f82
chusiping <chusping@163.com>
2026-03-09 11:22:46 +0800

    2026-3-9 11 :22 改成7533

--- Gps808_flask/app.py
+++ Gps808_flask/app.py
@@ -190,4 +190,4 @@ def delete_user():
     return jsonify({'success': False, 'message': '用户不存在'})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=7533)
