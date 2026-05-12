b1b309e0d40f7f4e7ab1e008f43e8be69aba8b89
chusiping <chusping@163.com>
2026-05-12 22:09:06 +0800

    自动固定区域截图

---
+++ 快捷键截图固定区域/autocutjpg.py
@@ -0,0 +1,22 @@
+import argparse
+import keyboard
+import mss
+import time
+import os
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-d', '--directory', default='output')
+args = parser.parse_args()
+
+output_dir = args.directory
+
+def shot():
+    os.makedirs(output_dir, exist_ok=True)
+    with mss.mss() as sct:
+        filename = os.path.join(output_dir, f"{int(time.time())}.jpg")
+        img = sct.grab(sct.monitors[1])
+        mss.tools.to_png(img.rgb, img.size, output=filename)
+
+keyboard.add_hotkey('F1', shot)
+
+keyboard.wait()
