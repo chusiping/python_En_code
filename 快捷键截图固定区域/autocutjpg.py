95edc1d62d1fa838ad1c656eca2329a1b637db2d
chusiping <chusping@163.com>
2026-06-11 21:56:25 +0800

    重命名文件

--- 快捷键截图固定区域/autocutjpg.py
+++ 快捷键截图固定区域/autocutjpg.py
@@ -1,8 +1,10 @@
 import argparse
 import keyboard
 import mss
+import mss.tools
 import time
 import os
+from datetime import datetime
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-d', '--directory', default='output')
@@ -12,11 +14,16 @@ output_dir = args.directory
 
 def shot():
     os.makedirs(output_dir, exist_ok=True)
+
     with mss.mss() as sct:
-        filename = os.path.join(output_dir, f"{int(time.time())}.jpg")
+        filename = os.path.join(
+            output_dir,
+            datetime.now().strftime("%Y%m%d_%H%M%S.jpg")
+        )
+
         img = sct.grab(sct.monitors[1])
         mss.tools.to_png(img.rgb, img.size, output=filename)
 
-keyboard.add_hotkey('F1', shot)
+keyboard.add_hotkey('F4', shot)
 
 keyboard.wait()
