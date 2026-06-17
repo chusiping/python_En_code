3a3d01dc6b2053c65fb3df5e511c6b5e63e81aba
chusiping <chusping@163.com>
2026-06-17 15:40:34 +0800

    复制到粘贴板

--- 快捷键截图固定区域/autocutjpg.py
+++ 快捷键截图固定区域/autocutjpg.py
@@ -1,30 +1,58 @@
 import argparse
 import keyboard
 import mss
-import mss.tools
-import time
-import os
-from datetime import datetime
+from PIL import Image
+from io import BytesIO
+import win32clipboard
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-d', '--directory', default='output')
 args = parser.parse_args()
 
-output_dir = args.directory
+
+def copy_image_to_clipboard(img):
+    # MSS截图转PIL图片
+    image = Image.frombytes(
+        "RGB",
+        img.size,
+        img.rgb
+    )
+
+    output = BytesIO()
+
+    # 剪贴板需要BMP格式，且去掉14字节文件头
+    image.convert("RGB").save(output, "BMP")
+    data = output.getvalue()[14:]
+
+    output.close()
+
+    win32clipboard.OpenClipboard()
+    try:
+        win32clipboard.EmptyClipboard()
+        win32clipboard.SetClipboardData(
+            win32clipboard.CF_DIB,
+            data
+        )
+    finally:
+        win32clipboard.CloseClipboard()
+
 
 def shot():
-    os.makedirs(output_dir, exist_ok=True)
-
     with mss.mss() as sct:
-        filename = os.path.join(
-            output_dir,
-            datetime.now().strftime("%Y%m%d_%H%M%S.jpg")
-        )
-        # img = sct.grab(sct.monitors[1])
-        region = {"left": 1, "top": 80, "width": 1910, "height": 910}
+        region = {
+            "left": 38,
+            "top": 113,
+            "width": 1230,
+            "height": 560
+        }
+
         img = sct.grab(region)
-        mss.tools.to_png(img.rgb, img.size, output=filename)
+        copy_image_to_clipboard(img)
+
+        print("截图已复制到剪贴板")
+
 
 keyboard.add_hotkey('F4', shot)
 
+print("按 F4 截图并复制到剪贴板")
 keyboard.wait()
