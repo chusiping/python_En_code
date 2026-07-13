9537acf3534226bbe39de0693ee7eb76c34339b1
chusiping <chusping@163.com>
2026-07-13 19:13:50 +0800

    split_hex 强化

--- Gps808_flask/vjt04090/codec.py
+++ Gps808_flask/vjt04090/codec.py
@@ -48,8 +48,49 @@ def phone_to_bcd(phone:str):
     return bytes(result)
 
 def split_hex(hexstr):
-    hexstr = hexstr.replace(" ", "").replace("\n", "")
-    return hexstr.upper()
+    """
+    JT/T808 解转义
+    输入: 十六进制字符串
+    例如: 02007D027D01AB
+    输出:
+    02007E7DAB
+    """
+    hexstr = hexstr.upper().replace(" ", "").replace("\n", "")
+    result = []
+    idx = 0
+
+    while idx < len(hexstr):
+
+        # 判断是否遇到7D
+        if hexstr[idx:idx+2] == "7D":
+
+            # 防止越界
+            if idx + 4 > len(hexstr):
+                raise ValueError("转义码不完整")
+
+            next_byte = hexstr[idx+2:idx+4]
+
+            if next_byte == "01":
+                # 7D01 -> 7D
+                result.append("7D")
+
+            elif next_byte == "02":
+                # 7D02 -> 7E
+                result.append("7E")
+
+            else:
+                # 非法转义
+                raise ValueError(
+                    f"非法转义: 7D{next_byte}"
+                )
+
+            idx += 4
+
+        else:
+            result.append(hexstr[idx:idx+2])
+            idx += 2
+
+    return "".join(result)
 
 def parse_bcd_time(hexstr):
     """
