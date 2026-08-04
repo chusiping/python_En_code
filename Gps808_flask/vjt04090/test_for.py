e0bf8748c506c85f6edc8c720f217e63c6bc14cf
chusiping <chusping@163.com>
2026-08-04 16:51:25 +0800

    yh

--- Gps808_flask/vjt04090/test_for.py
+++ Gps808_flask/vjt04090/test_for.py
@@ -2,6 +2,38 @@ import os
 import json
 import argparse
 from parser import parse_jt808_packet
+from save_result import * 
+import sys
+
+import re
+
+def find_unparsed_data(text):
+    """
+    查找字符串中疑似未解析的数据片段
+    特征：
+    - 英文字母 + 数字混合
+    - 连续长度 >= 4
+    """
+
+    pattern = r'\b(?=[A-Za-z0-9]*[A-Za-z])(?=[A-Za-z0-9]*\d)[A-Za-z0-9]+\b'
+
+    return re.findall(pattern, text)
+
+def find_unparsed_in_json(data):
+    result = []
+
+    if isinstance(data, dict):
+        for k, v in data.items():
+            result.extend(find_unparsed_in_json(v))
+    elif isinstance(data, list):
+        for v in data:
+            result.extend(find_unparsed_in_json(v))
+    elif isinstance(data, str):
+        # 英文数字混合
+        result.extend(find_unparsed_data(data))
+    return result
+
+
 
 def main():
     parser = argparse.ArgumentParser(
@@ -53,6 +85,12 @@ def main():
                 f"正在解析 {base_name} 第 {line_no} 行"
             )
             print(hexstr)
+
+            bad = find_unparsed_in_json(hexstr)
+            for x in bad:
+                print("发现未解析:", x)
+                hexstr
+
             try:
                 result=parse_jt808_packet(
                     hexstr
@@ -68,24 +106,7 @@ def main():
                 # ==========================
                 # 保存结果
                 # ==========================
-                out_file=os.path.join(
-                    result_dir,
-                    f"{base_name}_{line_no}.json"
-                )
-                with open(
-                    out_file,
-                    "w",
-                    encoding="utf-8"
-                ) as out:
-                    json.dump(
-                        result,
-                        out,
-                        ensure_ascii=False,
-                        indent=4
-                    )
-                print(
-                    f"\n保存:{out_file}"
-                )
+                filename = save_result(result)
             except Exception as e:
                 print(
                     "解析失败:",
@@ -103,4 +124,9 @@ def main():
                 )
                 break
 if __name__=="__main__":
+    sys.argv = [
+        "test_for.py",
+        "-f",
+        "测试包/0200_0803.txt"
+    ]
     main()
