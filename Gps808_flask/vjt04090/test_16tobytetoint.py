d9247dc6beb6415b2023d582a7ed06ff348a905a
chusiping <chusping@163.com>
2026-08-04 18:22:45 +0800

    yh

--- Gps808_flask/vjt04090/test_16tobytetoint.py
+++ Gps808_flask/vjt04090/test_16tobytetoint.py
@@ -1,15 +1,48 @@
-s = "0200" 
-b = bytes.fromhex(s) 
-n = int.from_bytes(b, "big")
-xn = int.from_bytes(b, "little")
-n2 = int.from_bytes(bytes.fromhex("0200"), "big")
+# s = "0200" 
+# print(f'两个十六进制字节: s={s}')
+
+# b = bytes.fromhex(s)
+# print(f'bytes.fromhex(s)  转成字节数据: b={b}') 
+
+# n = int.from_bytes(b, "big")
+# print(f'int.from_bytes(b, "big") 大端转整数: n={n}')
+
+# xn = int.from_bytes(b, "little")
+# print(f'int.from_bytes(b, "little") 小端转整数: xn={xn}')
+
+# n2 = int.from_bytes(bytes.fromhex("0200"), "big")
+# print(f'int.from_bytes直接转转整数: n={n}')
+
+# two = bin(n)
+# print(f'bin(n) 整数转二进制 0b表示这是二进制: two={two}')
+
+# n = 126
+# print(f"整数126：二进制 bin(126)={bin(n)}, 8位={n:08b}")
+# print(f"整数126：十六进制{hex(n)}") 
+
+# s = "7E"
+# num = int(s, 16)
+# print(f"7E十六进制：转整数{num}") 
+
+# s = "01111110"
+# num = int(s, 2)
+# print(f"01111110二进制：转整数{num}") 
 
 
-print(f'两个十六进制字节: s={s}')
-print(f'bytes.fromhex(s)  转成字节数据: b={b}')
-print(f'int.from_bytes(b, "big") 大端转整数: n={n}')
-print(f'int.from_bytes(b, "little") 小端转整数: xn={xn}')
-print(f'int.from_bytes直接转转整数: n={n}')
+# data = b'\x7e'
+# print(f"b\\x7e 字节转十六进制{data.hex()}") 
+
+# data = b'\x7e'
+# num = int.from_bytes(data, "big")
+# print(f"b\\x7e 字节转整数{num}") 
+
+
+# 126是字符串 转 二进制 和 十六进制 怎么写
+s = "126"
+n = int(s)
+print(f"字符串126 转整数 {n}") 
+print(f"字符串126 十六进制 {hex(n)}") 
+
 
 info = f"""
 1   JT808 收到的数据通常不是字符串
@@ -52,5 +85,11 @@ info = f"""
     data = bytes.fromhex(hexstr)
     因为 bytes.fromhex() 本身支持忽略空格
     
+
+    这三个表示的是同一个值：126 = 0b01111110 = 0x7E
+
+
+    字节是数据存储单位规定：1 byte = 8 bit
+    0111 1110 : 这 8 个二进制位，就是一个字节
 """
-print(info)
+# print(info)
